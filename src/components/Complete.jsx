62a67941b5c93626501dda38023d11d7e036511c
Julio S <85770548+Julius-Sol@users.noreply.github.com>
2023-03-11 17:29:07 -0500

    Finish desktop design for the complete component

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -3,7 +3,7 @@ import check from '../assets/icon-check.svg'
 
 export default function Complete(props) {
   return (
-    <div className={`${props.showComplete? "" : "hidden"} fixed flex flex-col -translate-x-1/2 left-[50%] w-[90%] mt-[8rem] py-3 px-5 bg-white items-center justify-center rounded-lg z-50`}>
+    <div className={`${props.showComplete? "" : "hidden"} fixed flex flex-col -translate-x-1/2 left-[50%] w-[90%] mt-[8rem] py-3 px-5 bg-white items-center justify-center rounded-lg z-50 tablet:max-w-[500px]`}>
       <img src={check} alt="check image" className='w-[64px]'/>
       <h1 className='font-bold text-lg mt-5'>Thanks for your support!</h1>
       <p className='text-sm text-Dark-gray mb-10 mt-5 text-center'>Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get an email once our campaign is completed.</p>
